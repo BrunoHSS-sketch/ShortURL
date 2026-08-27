ad851105ec2d2379cc6d6c9186919781be4bbdcd
Bruno.S <brunoschmitthenrique@gmail.com>
2026-08-27 13:42:12 -0300

    Add conection in database redis

---
+++ src/main/java/shorturl/repository/RedisUrlRepository.java
@@ -0,0 +1,22 @@
+package shorturl.repository;
+
+public class RedisUrlRepository implements UrlRepository{
+
+    private Jedis jedis = new Jedis("localhost", 6379);
+
+    @Override
+    public void saveUrl(String originalUrl, String shortUrl) {
+        jedis.set(originalUrl, shortUrl);
+        jedis.set(shortUrl, originalUrl);
+    }
+
+    @Override
+    public String getShortUrl(String originalUrl) {
+        return jedis.get(originalUrl);
+    }
+
+    @Override
+    public String getOriginalUrl(String shortUrl) {
+        return jedis.get(shortUrl);
+    }
+}
---
+++ src/main/java/shorturl/repository/UrlRepository.java
@@ -0,0 +1,7 @@
+package shorturl.repository;
+
+public interface UrlRepository {
+    public void saveUrl(String originalUrl, String shortUrl);
+    public String getShortUrl( String originalUrl);
+    public String getOriginalUrl(String shortUrl);
+}
